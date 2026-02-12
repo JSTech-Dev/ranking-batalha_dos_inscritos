bd18c615f667c878d67404c7c4159455355332a9
JSTech-Dev <tamireslara323@gmail.com>
2026-02-12 17:57:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
-// config.js
 const LISTA_DE_VIDEOS = [
     {
-        id: "v002",
-        titulo: "Batalha dos 500 - O Caos Total",
-        data: "12/02/2026",
-        arquivo: "dados/partida_02.json", // Nome do arquivo na pasta dados
-        thumb: "https://via.placeholder.com/300x160/000000/00ffcc?text=Video+02" // Pode por link de imagem real
+        id: "v003",
+        titulo: "Batalha do TikTok",
+        data: "14/02/2026",
+        arquivo: "dados/partida_tiktok.json",
+        // COLE O LINK DO TIKTOK (Pode ser vm.tiktok ou tiktok.com)
+        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7605784502082096391?lang=pt-BR" 
     },
     {
-        id: "v001",
-        titulo: "Primeira Batalha Oficial - JS Tech",
-        data: "10/02/2026",
-        arquivo: "dados/Ranking_Partida_20260211_234835.json",
-        thumb: "https://via.placeholder.com/300x160/000000/00ffcc?text=Video+01"
+        id: "v002",
+        titulo: "Batalha YouTube",
+        data: "12/02/2026",
+        arquivo: "dados/partida_02.json",
+        videoUrl: "https://youtu.be/..."
     }
-	
-    // Para adicionar novo, copie o bloco acima, cole e mude os dados.
 ];
