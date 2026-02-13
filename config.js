a653db8f7d35d8b395858818ae48b0e298beb2d5
JSTech-Dev <tamireslara323@gmail.com>
2026-02-13 00:10:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const LISTA_DE_VIDEOS = [
         titulo: "Batalha do TikTok (25k Jogadores)",
         data: "14/02/2026",
         arquivo: "dados/partida_tiktok.json",
-        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/732589...",
+        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7605980391157157121",
         
         // --- AQUI É O PULO DO GATO ---
         // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
@@ -13,4 +13,5 @@ const LISTA_DE_VIDEOS = [
         thumb: "" 
     }
 ];
+
 
