9970173c1f5007ffaaa26546b0dcdbd8f9a42964
JSTech-Dev <tamireslara323@gmail.com>
2026-02-12 23:39:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const LISTA_DE_VIDEOS = [
         id: "v003",
         titulo: "Batalha do TikTok (25k Jogadores)",
         data: "14/02/2026",
-        // O NOME TEM QUE SER IGUALZINHO AO DO ARQUIVO NA PASTA DADOS
-        arquivo: "dados/partida_tiktok.json",
+        // MUDANÇA AQUI: Tirei o "dados/" da frente
+        arquivo: "partida_tiktok.json", 
         
-        // Link do vídeo (opcional, se tiver o vídeo no TikTok)
-        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/732589..." 
+        // Link do vídeo do TikTok
+        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7325890000000000000" 
     }
 ];
