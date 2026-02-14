b5e6c2c86a4378b657ae8263d203d89cf3e4135e
JSTech-Dev <tamireslara323@gmail.com>
2026-02-14 01:16:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,6 @@
 // config.js
 const LISTA_DE_VIDEOS = [
     {
-        id: "v001",
-        titulo: "Dia 13 de Fevereiro de 2026 - Colocando os meus seguidores pra lutar até restar apenas 1",
-        data: "13/02/2026",
-        arquivo: "dados/13-02-2026-09.36.json",
-        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7606335698441932049",
-        
-        // --- AQUI É O PULO DO GATO ---
-        // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
-        // Se deixar vazio (""), ele tenta buscar sozinho.
-        thumb: "" 
-    },
-    {
         id: "v002",
         titulo: "14.02.26 Colocando meus seguidores pra lutar até restar apenas 1",
         data: "13/02/2026",
@@ -23,8 +11,21 @@ const LISTA_DE_VIDEOS = [
         // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
         // Se deixar vazio (""), ele tenta buscar sozinho.
         thumb: "" 
+    },
+    {
+        id: "v001",
+        titulo: "Dia 13 de Fevereiro de 2026 - Colocando os meus seguidores pra lutar até restar apenas 1",
+        data: "13/02/2026",
+        arquivo: "dados/13-02-2026-09.36.json",
+        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7606335698441932049",
+        
+        // --- AQUI É O PULO DO GATO ---
+        // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
+        // Se deixar vazio (""), ele tenta buscar sozinho.
+        thumb: "" 
     }
 ];
+
 
 
 
