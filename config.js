547b10c7c29767d90686d5c26002192c232c5ac6
JSTech-Dev <tamireslara323@gmail.com>
2026-02-14 01:14:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,21 @@ const LISTA_DE_VIDEOS = [
         // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
         // Se deixar vazio (""), ele tenta buscar sozinho.
         thumb: "" 
+    },
+    {
+        id: "v002",
+        titulo: "14.02.26 Colocando meus seguidores pra lutar até restar apenas 1",
+        data: "13/02/2026",
+        arquivo: "dados/14.02.26-12.48.json",
+        videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7606566950910577938?is_from_webapp=1&sender_device=pc&web_id=7587976151559554568",
+        
+        // --- AQUI É O PULO DO GATO ---
+        // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
+        // Se deixar vazio (""), ele tenta buscar sozinho.
+        thumb: "" 
     }
 ];
+
 
 
 
