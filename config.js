98faadf967ced8dd4e34d40ae2d3dbcb8373e440
JSTech-Dev <tamireslara323@gmail.com>
2026-02-15 03:02:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,21 @@
 // config.js
 const LISTA_DE_VIDEOS = [
     {
+        id: "v003",
+        titulo: "15.02.26 Colocando meus seguidores pra lutar até restar apenas 1",
+        data: "15/02/2026",
+        arquivo: "dados/Ranking_Partida_15-02-26_02.42.json",
+        videoUrl: "https://webapp-sg.tiktok.com/86ace82301767877247f13896b3f5bff/69940497/tos-alisg-pve-0037/ocagABfEegkcp0scrIBAVhAxu4QUFMCmDlkWwi?a=1988&bti=ODszNWYuMDE6&ch=0&cr=3&dr=0&lr=all&cd=0%7C0%7C0%7C&cv=1&br=4462&bt=2231&cs=0&ds=4&ft=bLZkAmrMPD12NNc_td-UxC~2KY6e3wv25ucAp&mime_type=video_mp4&qs=0&rc=cnF8b2hsc2d3SkBwaHIxaDFybndmOjdmMzgzNDk1ZGZpaDQ0OEBpMzNzeWw5cmdwOTMzODgzNEBjRl5Nc3FePmJKYSNvYF90aHFmOiMyMWMyYjMuNTYxYTU1MGAtYSNfLi1uMmQ0LTVhLS1kL2Bzcw%3D%3D&l=202602151401385A0092697B49814A9A70&btag=200090000&ply_type=3&policy=eyJ2bSI6MywidWlkIjoiNzI4NTMxMjIxMDQ3Njc3ODUwMSJ9",
+        
+        // --- AQUI É O PULO DO GATO ---
+        // Se você preencher isso aqui, o site carrega INSTANTANEAMENTE
+        // Se deixar vazio (""), ele tenta buscar sozinho.
+        thumb: "" 
+    },
+    {
         id: "v002",
         titulo: "14.02.26 Colocando meus seguidores pra lutar até restar apenas 1",
-        data: "13/02/2026",
+        data: "14/02/2026",
         arquivo: "dados/14.02.26-12.48.json",
         videoUrl: "https://www.tiktok.com/@batalha_dos_inscritos/video/7606566950910577938?is_from_webapp=1&sender_device=pc&web_id=7587976151559554568",
         
@@ -25,6 +37,7 @@ const LISTA_DE_VIDEOS = [
         thumb: "" 
     }
 ];
+
 
 
 
